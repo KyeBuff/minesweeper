821c5ac3920397bf68babdf884ae0038efa7af46
KyeBuff <kye.buffery@btinternet.com>
2018-01-17 09:43:59 +0000

    grid styled, render logic updated, comments added around gen logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-let columns = 10, rows = 10, mines = 5;
+let columns = 20, rows = 20, mines = 50;
 
 // Board state
 // 2D array which allows for easier tracking of mines from a given cell location
+// L1 - Board
+// L2 - Arrays representing rows, elements represent the columns
 let board = [];
 
 function generateMines() {
@@ -12,7 +14,7 @@ function generateMines() {
 		// Set x, y values which match indices in rows and columns
 			x = Math.floor(Math.random() * columns),
 			y = Math.floor(Math.random() * rows);
-		} while (board[y][x] === -1)
+		} while (board[y][x] === -1);
 		// -1 represents a mine
 		// if current cell is a mine regenerate x,y
 		board[y][x] = -1;
@@ -35,7 +37,7 @@ function inBounds(x, y) {
 	return x >= 0 && y >= 0 && x < columns && y < rows;
 }
 
-function countCloseMines() {
+function countNeighbourMines() {
 	// Need to check cells that are within 1 indices +/-
 	//dx/dy = difference on both axis
 	for(let y=0; y<rows; y++) {
@@ -45,8 +47,23 @@ function countCloseMines() {
 				continue;
 			} 
 			let count = 0;
-			// Count # of mines around the cell (X, Y)
-			// These additonal loops allow us to look at the direct cells around the current mine by checking all cells that exist within 1 index around on Y/X
+			// Count # of mines around the cell (y,x)
+			// These additonal loops allow us to look at the direct cells around the current mine by checking all cells that exist within 1 index around on (y,x)
+
+			/*
+
+				Visualisation of dx, dy for any given cell. Takes the y,x values of the current cell and traverses
+
+				***********************
+				* -1 -1 * 0 -1 * 1 -1 *
+				***********************		
+				*	-1  0 * 0  0 * 1  0 *
+				***********************
+				* -1  1 * 0  1 * 1  1 *
+				***********************
+
+			*/
+
 			for(let dy=-1; dy<=1; dy++) {
 				for(let dx=-1; dx<=1; dx++) {
 					//We do not need to check itself.
@@ -62,7 +79,7 @@ function countCloseMines() {
 					}
 				}		
 			}
-			board[y][x] = board[y][x] === -1 ? board[y][x] :count;
+			board[y][x] = board[y][x] === -1 ? board[y][x] : count;
 		}
 	}	
 }
@@ -73,7 +90,9 @@ function init() {
 
 	generateMines();
 
-	countCloseMines();
+	countNeighbourMines();
+
+	render();
 
 }
 
@@ -92,11 +111,46 @@ function render() {
 
 			const cell = document.createElement('div');
 
-			cell.textContent = board[y][x] === -1 ? "X" : board[y][x];
-
-			cell.style.width = (100 / columns) + "%";
-
 			cell.className = "cell";
+
+			switch(board[y][x]) {
+				case -1: 
+					cell.classList.add("cell-mine");
+					break;
+				case 1: 
+					cell.classList.add("cell-1");
+					break;
+				case 2: 
+					cell.classList.add("cell-2");
+					break;
+				case 3: 
+					cell.classList.add("cell-3");
+					break;
+				case 4: 
+					cell.classList.add("cell-4");
+					break;
+				case 5: 
+					cell.classList.add("cell-5");
+					break;
+				case 6: 
+					cell.classList.add("cell-6");
+					break;
+				case 7: 
+					cell.classList.add("cell-7");
+					break;
+				case 8: 
+					cell.classList.add("cell-8");
+					break;
+				default:
+					cell.classList.add("cell-empty");
+					break;
+			}
+
+			if(board[y][x] > 0) {
+				cell.textContent = board[y][x];
+			} 
+
+			// cell.style.width = (100 / columns) + "%";
 
 			row.append(cell);
 		}
@@ -112,8 +166,8 @@ document.getElementById('form').addEventListener("submit", (e) => {
 	e.preventDefault();
 
 	const newCols = document.getElementById("cols").value,
-	newRows = document.getElementById("rows").value,
-	newMines = document.getElementById("mines").value;
+				newRows = document.getElementById("rows").value,
+				newMines = document.getElementById("mines").value;
 	
 	columns = +newCols;
 	rows = +newRows;
@@ -123,6 +177,6 @@ document.getElementById('form').addEventListener("submit", (e) => {
 	init();
 	render();
 
-	console.log(board);
-
 });
+
+init();
